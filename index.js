68a1331f6986f8db595727b419ced275a93bfe07
Vignesh786 <98738911+vigneshmrao@users.noreply.github.com>
2024-07-10 13:55:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,41 @@
-const express = require('express')
+const express = require('express');
+const AWS = require('aws-sdk');
 const app = express();
-const port =3000;
-const AWS = require('aws-sdk'); 
- 
+const port = 3000;
+
+// Initialize the AWS Secrets Manager client
 const secretsManager = new AWS.SecretsManager({
-    region: 'ap-south-1' // e.g., 'us-west-2'
+    region: 'ap-south-1' // Make sure this is your actual AWS region
 });
- 
+
 // Function to get secret value
 const getSecretValue = async (secretName) => {
-    console.log("===========");
     try {
-        
         const data = await secretsManager.getSecretValue({ SecretId: secretName }).promise();
-        console.log("Data :",data);
         if ('SecretString' in data) {
-            console.log("SecretString: ",data);
             return data.SecretString;
         } else {
             let buff = Buffer.from(data.SecretBinary, 'base64');
             return buff.toString('ascii');
         }
     } catch (err) {
-        console.error(err);
+        console.error('Failed to retrieve secret:', err);
         return null;
     }
 };
- 
- API endpoint to print the secret value
- app.get('/secret/:name', async (req, res) => {
-     console.log("Input-->",req.params);
-     const secretName = req.params.name;
-     const secretValue = await getSecretValue(secretName);
+
+// API endpoint to print the secret value
+app.get('/secret/:name', async (req, res) => {
+    const secretName = req.params.name;
+    const secretValue = await getSecretValue(secretName);
     
-     if (secretValue) {
-         res.send(`The value of the secret ${secretName} is: ${secretValue}`);
-     } else {
-         res.status(500).send('Failed to retrieve the secret value');
-     }
- });
- 
-
-getSecretValue("demo-secret")
-
+    if (secretValue) {
+        res.send(`The value of the secret ${secretName} is: ${secretValue}`);
+    } else {
+        res.status(500).send('Failed to retrieve the secret value');
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
- 
-
