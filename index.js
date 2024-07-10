23cb45c9f4935dd9406a90a920c78dd97168a2fb
root <root@ubuntu.myguest.virtualbox.org>
2024-07-10 12:23:18 +0530

    added new code

--- index.js
+++ index.js
@@ -1,45 +1,53 @@
-const express = require('express');
-const logger = require('./logger');
-const { fetchData } = require('./secret-manager-dev/index');
-
+const express = require('express')
 const app = express();
-const port = 3000;
+const port =3000;
+const AWS = require('aws-sdk');
+ 
 
-app.use(express.json());
-
-app.use((req, res, next) => {
-  logger.info(`Incoming request: ${req.method} ${req.url}`);
-  next();
+ 
+const secretsManager = new AWS.SecretsManager({
+    region: 'ap-southeast-1' // e.g., 'us-west-2'
 });
+ 
+// Function to get secret value
+const getSecretValue = async (secretName) => {
+    console.log("===========");
+    try {
+        
+        const data = await secretsManager.getSecretValue({ SecretId: secretName }).promise();
+        console.log("Data :",data);
+        if ('SecretString' in data) {
+            console.log("SecretString: ",data);
+            return data.SecretString;
+        } else {
+            let buff = Buffer.from(data.SecretBinary, 'base64');
+            return buff.toString('ascii');
+        }
+    } catch (err) {
+        console.error(err);
+        return null;
+    }
+};
+ 
+// API endpoint to print the secret value
+// app.get('/secret/:name', async (req, res) => {
+//     console.log("Input-->",req.params);
+//     const secretName = req.params.name;
+//     const secretValue = await getSecretValue(secretName);
+    
+//     if (secretValue) {
+//         res.send(`The value of the secret ${secretName} is: ${secretValue}`);
+//     } else {
+//         res.status(500).send('Failed to retrieve the secret value');
+//     }
+// });
+ 
 
-fetchData().then(() => {
-  console.log('Data fetched and environment variables set.');
-}).catch((error) => {
-  console.error('Failed to fetch data:', error);
-});
+getSecretValue("demo-secret")
 
-app.get('/', (req, res) => {
-  let sampleData = {
-    "user": process.env.DB_USER,
-    "host": process.env.DB_HOST,
-    "port": process.env.DB_PORT,
-    "database": process.env.DB_NAME
-  };
-  res.send(sampleData);
-});
-
-app.post('/data', (req, res) => {
-  const data = { "CLIENT_TOKEN": process.env.CLIENT_TOKEN };
-  logger.info(`Data received: ${JSON.stringify(data)}`);
-  res.send(`You sent: ${JSON.stringify(data)}`);
-});
-
-app.use((err, req, res, next) => {
-  logger.error(`Error: ${err.message}`);
-  res.status(500).send('Something broke!');
-});
 
 app.listen(port, () => {
-  logger.info(`API is running at http://localhost:${port}`);
+    console.log(`Server running on http://localhost:${port}`);
 });
+ 
 
